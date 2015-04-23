2702410fb1d6a633cf46960b33ddfb8af3c0fae3
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2015-04-23 16:58:08 +0100

    added model tag

---
+++ app/models/tag.rb
@@ -0,0 +1,17 @@
+class Tag < ActsAsTaggableOn::Tag
+  has_many :slugs, as: :sluggable, dependent: :destroy
+  extend FriendlyId
+  friendly_id :slug_candidates
+  def slug_candidates
+    [:name]
+  end
+
+  after_save :create_slug
+
+  def create_slug
+    return if !slug_changed? || slug == slugs.last.try(:slug)
+    previous = slugs.where('lower(slug) = ?', slug.downcase)
+    previous.delete_all
+    slugs.create!(slug: slug)
+  end
+end
