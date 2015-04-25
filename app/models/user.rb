695e78442da8bb7cd61e1ac526a01a5e7be7b5c2
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2015-04-26 00:13:33 +0100

    removed authentication concern

--- app/models/user.rb
+++ app/models/user.rb
@@ -101,6 +101,23 @@ class User < ActiveRecord::Base
     balance > amount.to_i
   end
 
+  #Login using both email and username
+  def login=(login)
+    @login = login
+  end
+
+  def login
+    @login || self.username || self.email
+  end
+
+  def email_required?
+    super && authentications.blank?
+  end
+
+  def password_required?
+    super && authentications.blank?
+  end
+
   def send_devise_notification(notification, *args)
     devise_mailer.send(notification, self, *args).deliver_later
   end
@@ -161,5 +178,17 @@ class User < ActiveRecord::Base
   def decrement_counters
     school.students_counter.decrement
   end
+
+  protected
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      where(conditions).first
+    end
+  end
+
 
 end
