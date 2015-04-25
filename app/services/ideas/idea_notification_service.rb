8263fa81009f242a93c25308aa5197eb96c2bbe6
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2015-04-26 00:00:28 +0100

    idea notification service

---
+++ app/services/ideas/idea_notification_service.rb
@@ -0,0 +1,40 @@
+class IdeaNotificationService
+
+  def initialize(idea, user)
+    @idea = feedback.idea
+    @user = feedback.user
+  end
+
+  def notify
+    if activity_exists?
+      publish_activity
+    else
+      create_activity
+    end
+  end
+
+  private
+
+  #Check if activity exists for idea?
+  def activity_exists?
+    Activity.where(trackable_id: @idea.id, key: "create").exists?
+  end
+
+  #Create activity if new idea
+  def create_activity
+    @activity = @user.activities.create!(trackable: @idea, recipient: @idea, key: 'create', verb: 'pitched')
+    PublishIdeaJob.set(wait: 15.seconds).perform_later(@idea.id, @user.id, @activity.id)
+  end
+
+  #Publish activity if activity exists?
+  def publish_activity
+    Activity.where(trackable_id: @idea.id, trackable_type: "Idea").find_each do |activity|
+      if activity && !activity.published?
+        activity.published = true
+        activity.save!
+        true
+      end
+    end
+  end
+
+end
