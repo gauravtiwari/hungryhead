c3c68c6035c8589b01cd21dd4fcbdf56fef13c60
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2015-04-27 12:45:41 +0100

    updated unique columns index

--- db/migrate/20140825003938_create_activities.rb
+++ db/migrate/20140825003938_create_activities.rb
@@ -3,17 +3,17 @@ class CreateActivities < ActiveRecord::Migration
   # Create table
   def self.up
     create_table :activities do |t|
-      t.belongs_to :trackable, :polymorphic => true, unique: true
+      t.belongs_to :trackable, :polymorphic => true
       t.belongs_to :user
-      t.string  :key, unique: true
+      t.string  :key
       t.jsonb    :parameters, default: "{}"
-      t.boolean :published, default: true
+      t.boolean :published, default: true, index: true
       t.belongs_to :recipient, :polymorphic => true
       t.timestamps null: false
     end
 
-    add_index :activities, [:trackable_id, :trackable_type]
-    add_index :activities, [:key, :published, :type]
+    add_index :activities, [:trackable_id, :trackable_type], unique: true
+    add_index :activities, [:key], unique: true
     add_index :activities, [:recipient_id, :recipient_type]
   end
   # Drop table
--- db/migrate/20140825003939_create_notifications.rb
+++ db/migrate/20140825003939_create_notifications.rb
@@ -3,17 +3,17 @@ class CreateNotifications < ActiveRecord::Migration
   # Create table
   def self.up
     create_table :notifications do |t|
-      t.belongs_to :trackable, :polymorphic => true, unique: true
+      t.belongs_to :trackable, :polymorphic => true
       t.belongs_to :user
-      t.string  :key, unique: true
+      t.string  :key
       t.jsonb    :parameters, default: "{}"
-      t.boolean :published, default: true
+      t.boolean :published, default: true, index: true
       t.belongs_to :recipient, :polymorphic => true
       t.timestamps null: false
     end
 
-    add_index :notifications, [:trackable_id, :trackable_type]
-    add_index :notifications, [:key, :published]
+    add_index :notifications, [:trackable_id, :trackable_type], unique: true
+    add_index :notifications, [:key], unique: true
     add_index :notifications, [:recipient_id, :recipient_type]
   end
   # Drop table
