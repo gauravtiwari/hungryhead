f17bb929b7d6a9da48fc2f2e9beaaf90ce3b158d
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2015-04-27 14:08:11 +0100

    feedable concern and cache for activities

---
+++ app/models/concerns/feedable.rb
@@ -0,0 +1,18 @@
+module Feedable
+  extend ActiveSupport::Concern
+  included do
+    # Define polymorphic association to the parent
+    belongs_to :trackable, :polymorphic => true
+    # Define ownership to a resource responsible for this activity
+    belongs_to :user
+    # Define ownership to a resource targeted by this activity
+    belongs_to :recipient, :polymorphic => true
+    # Serialize parameters Hash
+    serialize :parameters, HashSerializer
+    #Serialize JSON
+    store_accessor :parameters, :verb, :meta
+    #reading notifications and activities
+    acts_as_readable :on => :created_at
+  end
+
+end
