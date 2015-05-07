195109ecd29cb3c46adcff7f774d445f40825c47
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2015-05-07 23:34:00 +0100

    latest feeds and users

---
+++ app/assets/javascripts/components/global/latest_content/latest_content_item.js.jsx
@@ -0,0 +1,17 @@
+var LatestContentItem = React.createClass({
+  render: function() {
+    return (
+      <li className="m-b-10">
+        <div className="widget-16-header p-b-10 p-l-15">
+        <div className="pull-left">
+            <a href={this.props.item.url}>
+              <p className="all-caps bold  small no-margin overflow-ellipsis ">{this.props.item.name}</p>
+            </a>
+            <p className="small no-margin">{this.props.item.pitch}</p>
+        </div>
+        <div className="clearfix"></div>
+        </div>
+      </li>
+    );
+  }
+});
