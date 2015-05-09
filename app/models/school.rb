298b594862459bafa52e89bc99110581e112e4c1
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2015-05-09 20:25:24 +0100

    school rb

--- app/models/school.rb
+++ app/models/school.rb
@@ -90,7 +90,7 @@ class School < ActiveRecord::Base
 
 	def remove_from_soulmate
 		loader = Soulmate::Loader.new("universities")
-	  	loader.remove("id" => id)
+	  loader.remove("id" => id)
 	end
 
 end
