21916c9dbf70155c0165eae9c6eed33b16354ea1
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2015-05-14 22:24:27 +0100

    renamed files with note to post

---
+++ app/assets/javascripts/components/global/latest_feed/latest_feed_post_item.js.jsx
@@ -0,0 +1,34 @@
+
+var LatestFeedPostItem = React.createClass({
+  render: function() {
+    var html_id = "feed_"+this.props.item.id;
+    if(window.currentUser.name === this.props.item.actor.actor_name) {
+      var actor = "You";
+    } else {
+      var actor = this.props.item.actor.actor_name;
+    }
+
+    if(this.props.item.actor.actor_avatar) {
+      var placeholder = <img src={this.props.item.actor.actor_avatar} width="32" height="32" />
+    } else {
+      var placeholder = <span className="placeholder no-padding bold text-white">{this.props.item.actor.actor_name_badge}
+              </span>;
+    }
+
+    return (
+        <li id={html_id} className="pointer p-b-10 p-t-10 fs-13 clearfix">
+          <span className="inline text-master">
+            <a className="text-complete" href={this.props.item.actor.url}>
+              <div className="thumbnail-wrapper fs-11 d24 user-pic circular inline m-r-10">
+                {placeholder}
+              </div>
+              <strong>{actor}</strong>
+            </a>
+            <span className="verb p-l-5">
+              shared a note
+            </span>
+          </span>
+        </li>
+      );
+  }
+});
