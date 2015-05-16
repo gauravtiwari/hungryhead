4ad685211ac775aeb6fb5b752703637d642a9331
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2015-05-16 17:24:10 +0100

    added bages controller and veiws

---
+++ app/controllers/badges_controller.rb
@@ -0,0 +1,12 @@
+class BadgesController < ApplicationController
+
+  before_action :authenticate_user!
+
+  layout "home"
+
+  def show
+    @badge = Merit::Badge.by_name(params[:id]).first
+    @users = User.find(Merit::BadgesSash.where(badge_id: @badge.id).pluck(:sash_id)).paginate(:page => params[:page], :per_page => 20)
+  end
+
+end
