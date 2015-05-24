aba7f8881650fb91937811d032816da5fa03c246
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2015-05-24 23:49:14 +0100

    added parent id to notifications

--- db/structure.sql
+++ db/structure.sql
@@ -849,6 +849,7 @@ CREATE TABLE notifications (
     trackable_id integer NOT NULL,
     trackable_type character varying NOT NULL,
     user_id integer NOT NULL,
+    parent_id integer,
     key character varying DEFAULT ''::character varying NOT NULL,
     parameters jsonb DEFAULT '{}'::jsonb,
     published boolean DEFAULT true,
