45d9c01b425594e7f72db19c5022140d2bdf6cf5
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2015-05-28 00:41:41 +0100

    added staging file

---
+++ config/environments/staging.rb
@@ -0,0 +1,67 @@
+Rails.application.configure do
+  # Settings specified here will take precedence over those in config/application.rb.
+
+  # In the development environment your application's code is reloaded on
+  # every request. This slows down response time but is perfect for development
+  # since you don't have to restart the web server when you make code changes.
+  config.cache_classes = true
+
+  # Do not eager load code on boot.
+  config.eager_load = true
+
+  # Show full error reports and disable caching.
+  config.consider_all_requests_local       = true
+  config.action_controller.perform_caching = true
+  config.cache_store = :mem_cache_store
+
+  # Don't care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = false
+
+  # Print deprecation notices to the Rails logger.
+  config.active_support.deprecation = :log
+
+  # Raise an error on page load if there are pending migrations.
+  config.active_record.migration_error = :page_load
+
+  config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+  config.assets.precompile += %w( .svg .eot .woff .ttf)
+
+  # Debug mode disables concatenation and preprocessing of assets.
+  # This option may cause significant delays in view rendering with a large
+  # number of complex assets.
+  config.assets.debug = false
+
+  config.react.addons = true
+
+  config.logger = Logger.new(STDOUT)
+  config.logger.level = Logger.const_get(
+    ENV['LOG_LEVEL'] ? ENV['LOG_LEVEL'].upcase : 'DEBUG'
+  )
+
+  config.active_record.raise_in_transactional_callbacks = true
+
+  # Enable serving of images, stylesheets, and JavaScripts from an asset server.
+
+  # Adds additional error checking when serving assets at runtime.
+  # Checks for improperly declared sprockets dependencies.
+  # Raises helpful error messages.
+  config.assets.raise_runtime_errors = true
+
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { host: '46.101.22.180' }
+  Rails.application.routes.default_url_options = { host: '46.101.22.180' }
+
+  config.action_mailer.smtp_settings = {
+    :address   => "smtp.mandrillapp.com",
+    :port      => 587,
+    :user_name => ENV["MANDRILL_USERNAME"],
+    :password  => ENV["MANDRILL_API_KEY"],
+    :enable_starttls_auto => true,
+    :authentication => 'login',
+    :domain => 'hungryhead.org',
+  }
+
+
+  # Raises error for missing translations
+  # config.action_view.raise_on_missing_translations = true
+end
