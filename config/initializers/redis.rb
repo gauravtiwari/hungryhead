63332791a131dd7f226604215238cbc29829ac85
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2015-05-28 18:53:28 +0100

    removed devise async and modeified redis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,3 @@
-location = 'redis://' + ENV["REDIS_ADDRESS"] || 'redis://127.0.0.1:6379/0'
+location = "redis://#{ENV['$REDIS_ADDRESS']}:6379/0/cache" || 'redis://127.0.0.1:6379/0/cache'
 uri = URI.parse(location)
 $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
--- config/initializers/soulmate.rb
+++ config/initializers/soulmate.rb
@@ -1,1 +1,3 @@
-Soulmate.redis = $redis
+location = "redis://#{ENV['$REDIS_ADDRESS']}:6379/0/soulmate" || 'redis://127.0.0.1:6379/0/soulmate'
+uri = URI.parse(location)
+Soulmate.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
