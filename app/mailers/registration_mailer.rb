90e0d7f06041ef6e84dab9d1ee03580520345751
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2015-05-29 20:42:22 +0100

    updated urls for hungryhead.co

--- app/mailers/registration_mailer.rb
+++ app/mailers/registration_mailer.rb
@@ -1,5 +1,5 @@
 class RegistrationMailer < ActionMailer::Base
-  default from: "admin@hungryhead.org"
+  default from: "no-reply@hungryhead.co"
   def welcome_email(user, password)
     @user = user
     @password = password
