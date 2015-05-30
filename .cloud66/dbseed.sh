b8d12b67f103b0807685370fe840cd88fe9bb0ee
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2015-05-30 22:51:55 +0100

    updated deploy hooks to reset database - 3

--- .cloud66/dbseed.sh
+++ .cloud66/dbseed.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd $STACK_PATH
-ps xa | grep postgres: | grep $POSTGRESQL_DATABASE | grep -v grep | awk '{print $1}' | sudo xargs kill
+ps xa | grep postgres: | grep $POSTGRESQL_DATABASE | grep -v grep | awk '{print $1}' | sudo xargs kill -9
 rake db:drop && rake db:create && rake db:migrate
 bundle exec rake db:seed
