52a06353cdd0f4695834f81e3cfd08ffb238f715
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2015-05-30 21:42:23 +0100

    fixed asset pipeline issue

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,7 +2,7 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
-Rails.application.config.assets.precompile += %w( core/css/windows.chrome.fix )
+Rails.application.config.assets.precompile += %w( core/css/windows.chrome.fix.css )
 Rails.application.config.assets.precompile += %w(*.svg *.eot *.woff *.ttf *.gif *.png *.ico)
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
