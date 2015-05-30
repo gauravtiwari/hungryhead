7ca5718c95e851330111bb644bdc29318870ae35
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2015-05-30 14:56:59 +0100

    fixing again the asset pipeline compilation -  4

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,7 +9,6 @@ Rails.application.config.assets.precompile += %w( icons/noti-cross.png)
 Rails.application.config.assets.precompile += %w( icons/top-tray.png)
 Rails.application.config.assets.precompile += %w( icons/top-tray_2x.png)
 Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
-Rails.application.config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif *.css *.js)
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
