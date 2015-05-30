5184bd036f141832d65ba93db9da6a6334fd0d3f
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2015-05-31 00:16:00 +0100

    fixed caching for scorable - added deployment hooks

--- .cloud66/dbseed.sh
+++ .cloud66/dbseed.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 cd $STACK_PATH
-ps xa | grep postgres: | grep $POSTGRESQL_DATABASE | grep -v grep | awk '{print $1}' | sudo xargs kill -9
-bundle exec rake production db:drop && bundle exec rake db:create && bundle exec rake db:migrate
 bundle exec rake db:seed
