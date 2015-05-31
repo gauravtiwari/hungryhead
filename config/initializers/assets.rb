ce6a47a2b91b65402f68dab46aa2b43783145b80
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2015-05-31 19:24:28 +0100

    fixed layout structuring - assets compilation

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,6 +3,7 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( core/css/windows.chrome.fix.css )
+Rails.application.config.assets.precompile += %w( core/css/ie9.css )
 Rails.application.config.assets.precompile += %w(*.svg *.eot *.woff *.ttf *.gif *.png *.ico)
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
