ea3d21f424333b26fbf63881ccf8da8f9f20e2f8
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2015-06-10 18:20:03 +0100

    join as mentor form update

--- app/assets/javascripts/components/mentor/join_as_mentor/join_as_mentor_form.js.jsx
+++ app/assets/javascripts/components/mentor/join_as_mentor/join_as_mentor_form.js.jsx
@@ -2,22 +2,28 @@ var JoinAsMentorForm = React.createClass({
 
   getInitialState: function() {
     return {
-      submitted: $.cookie('submitted_mentor_request')
+      submitted: $.cookie('submitted_mentor_request') ? $.cookie('submitted_mentor_request') : false,
+      name: $.cookie('submitted_mentor_request') ? $.cookie('submitted_mentor_name') : null,
+      loading: false
     }
   },
 
   submitInviteRequest: function(e) {
     e.preventDefault();
     if($(this.refs.form.getDOMNode()).valid()) {
+      this.setState({loading: true});
       var formData = $( this.refs.form.getDOMNode() ).serialize();
       event.stopPropagation();
       $.post(Routes.invite_requests_path(), formData, function(data, textStatus, xhr) {
         if(data.created) {
-          this.setState({submitted: true});
+          this.setState({submitted: true, name: data.name});
           $.cookie('submitted_mentor_request', true, {expires: 365});
+          $.cookie('submitted_mentor_name', data.name, {expires: 365});
+          this.setState({loading: false});
         }
       }.bind(this)).fail(function(xhr, textStatus, errorThrown) {
         var errors = JSON.parse(xhr.responseText);
+        this.setState({loading: false});
         $.each(errors, function(keys, values) {
           $('body').pgNotification({style: "simple", message: (values).toString(), position: "top-right", type: "danger",timeout: 5000}).show();
         });
@@ -26,35 +32,49 @@ var JoinAsMentorForm = React.createClass({
 
   },
 
+  clearCookie: function() {
+    $.removeCookie('submitted_mentor_request');
+    $.removeCookie('submitted_mentor_name');
+    this.setState({submitted: false, name: null});
+  },
+
   render: function() {
 
-    if(this.state.submitted === undefined) {
-      var content =  <form ref="form" noValidate="novalidate" className="new_invite_request" id="new_invite_request" acceptCharset="UTF-8" onSubmit={this.submitInviteRequest}>
-        <div className="col-md-4 auto-margin">
+    var cx = React.addons.classSet;
+    var classes = cx({
+      'fa fa-spinner fa-spin': this.state.loading
+    });
+
+    if(this.state.submitted) {
+      var content = <h3 className="text-center text-master wrapper-small">Thank you <span className="bold">{this.state.name} <i className="fa fa-smile-o"></i> </span> <br/>Your invite request has been submitted. We will be in touch. <br/> Not you? <a className="pointer" onClick={this.clearCookie}>Click here</a>  </h3>
+
+    } else {
+      var content =  <form ref="form" noValidate="novalidate" className="new_invite_request sm-p-b-50 p-b-20" id="new_invite_request" acceptCharset="UTF-8" onSubmit={this.submitInviteRequest}>
+        <div className="col-md-4 col-sm-6 auto-margin">
            <h3 className="text-master m-b-20 p-b-5 b-b b-grey pull-left">Lets get started</h3>
        </div>
        <div className="join-mentor-form clearfix">
-        <div className="col-sm-4 auto-margin">
+        <div className="col-sm-6 col-md-4 auto-margin">
           <div className="form-group form-group-default">
             <label>Your Name</label>
             <input className="string required form-control" placeholder="John Smith" type="text" name="invite_request[name]" id="invite_request_name" />
           </div>
         </div>
 
-        <div className="col-sm-4 auto-margin">
+        <div className="col-sm-6 col-md-4 auto-margin">
           <div className="form-group form-group-default">
             <label>Reference website</label>
             <input className="string url required form-control" placeholder="http:// linkedin, about me url..." type="url" name="invite_request[url]" id="invite_request_url" />
           </div>
         </div>
 
-        <div className="col-sm-4 auto-margin">
+        <div className="col-sm-6 col-md-4 auto-margin">
           <div className="form-group form-group-default input-group input-group-attached col-xs-12">
           <label>Email Address</label>
           <input className="string email required form-control" placeholder="johndoe@example.com" type="email" name="invite_request[email]" id="invite_request_email" />
           </div>
           <span className="input-group-btn">
-            <button type="submit" className="btn btn-complete pull-right btn-cons">Request invite</button>
+            <button type="submit" className="btn btn-complete pull-right btn-cons">Request invite <i className={classes}></i> </button>
           </span>
           <p className="fs-12 pull-right text-right m-t-20 text-master">
             Be first to find out when we Launch our product.
@@ -62,8 +82,6 @@ var JoinAsMentorForm = React.createClass({
         </div>
       </div>
       </form>;
-    } else {
-      var content = <h3 className="text-center text-master">Your invite request has been submitted. We will be in touch.</h3>
     }
 
     return (
