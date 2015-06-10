1029d819344c70d126709e3f9b034c5750a28fe4
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2015-06-10 18:23:07 +0100

    added help migrations

---
+++ db/migrate/20150610172254_create_help_categories.help.rb
@@ -0,0 +1,12 @@
+# This migration comes from help (originally 20150608203724)
+class CreateHelpCategories < ActiveRecord::Migration
+  def change
+    create_table :help_categories do |t|
+      t.string :name, null: false
+      t.string :slug, null: false
+
+      t.timestamps null: false
+    end
+    add_index :help_categories, :slug, unique: true
+  end
+end
---
+++ db/migrate/20150610172255_create_help_articles.help.rb
@@ -0,0 +1,14 @@
+# This migration comes from help (originally 20150608203817)
+class CreateHelpArticles < ActiveRecord::Migration
+  def change
+    create_table :help_articles do |t|
+      t.string :title
+      t.text :body
+      t.string :slug, null: false
+      t.references :category, index: true, foreign_key: true
+
+      t.timestamps null: false
+    end
+    add_index :help_articles, :slug, unique: true
+  end
+end
