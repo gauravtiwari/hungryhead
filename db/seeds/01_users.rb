8fdd00586b0ceeffe45031680ab57fc762777c0b
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2015-06-11 10:06:10 +0100

    added seeds files

--- db/seeds/01_users.rb
+++ db/seeds/01_users.rb
@@ -13,8 +13,140 @@ ActiveRecord::Base.transaction do
     email: 'admin@hungryhead.org',
     admin: true,
     role: 0,
-    state: 0,
     confirmed_at: Time.now
   )
+
+
+  Student.create!(
+    name: "Gaurav Tiwari",
+    first_name: "Gaurav",
+    last_name: "Tiwari",
+    username: "gaurav",
+    password: 'password',
+    mini_bio: Forgery::LoremIpsum.words(5),
+    school_id: 1,
+    location_list: "Lancaster",
+    email: "gaurav@gauravtiwari.co.uk",
+    fund: {balance: 1000},
+    role: 1,
+    market_list: "Education, Social, Entrepreneruship",
+    settings: {theme: 'solid', idea_notifications: true, feedback_notifications: true, investment_notifications: true, follow_notifications: true, post_notifications: true, weekly_mail: true},
+    confirmed_at: Time.now
+  )
+
+  Teacher.create!(
+    name: "Parul Singh",
+    first_name: "Parul",
+    last_name: "Singh",
+    username: "parul",
+    password: 'password',
+    mini_bio: Forgery::LoremIpsum.words(5),
+    school_id: 1,
+    location_list: "Lancaster",
+    email: "parul.rhl@gmail.com",
+    fund: {balance: 1000},
+    verified: true,
+    role: 4,
+    market_list: "Education, Social, Ecommerce",
+    settings: {theme: 'primary', idea_notifications: true, feedback_notifications: true, investment_notifications: true, follow_notifications: true, post_notifications: true, weekly_mail: true},
+    confirmed_at: Time.now
+  )
+
+  Mentor.create!(
+    name: "Stuart Logan",
+    first_name: "Stuart",
+    last_name: "Logan",
+    username: "stuart",
+    password: 'password',
+    mini_bio: Forgery::LoremIpsum.words(5),
+    school_id: 2,
+    location_list: "Manchester",
+    email: "stuart@hungryhead.org",
+    fund: {balance: 1000},
+    verified: true,
+    role: 3,
+    market_list: "Education, Entrepreneruship, Music",
+    settings: {theme: 'danger', idea_notifications: true, feedback_notifications: true, investment_notifications: true, follow_notifications: true, post_notifications: true, weekly_mail: true},
+    confirmed_at: Time.now
+  )
+
+  Mentor.create!(
+    name: "Damien Sheils",
+    first_name: "Damien",
+    last_name: "Sheils",
+    username: "damien",
+    mini_bio: Forgery::LoremIpsum.words(5),
+    password: 'password',
+    school_id: 2,
+    location_list: "Manchester",
+    email: "damien@hungryhead.org",
+    fund: {balance: 1000},
+    verified: true,
+    role: 3,
+    market_list: "Social, Entrepreneruship, Music",
+    settings: {theme: 'danger', idea_notifications: true, feedback_notifications: true, investment_notifications: true, follow_notifications: true, post_notifications: true, weekly_mail: true},
+    confirmed_at: Time.now
+  )
+
+  1.upto(30) { |i|
+
+    Mentor.create!(
+      name: Forgery::Name.full_name,
+      first_name: Forgery::Name.first_name,
+      last_name: Forgery::Name.last_name,
+      username: Forgery::Internet.user_name + SecureRandom.hex(4),
+      password: 'password',
+      mini_bio: Forgery::LoremIpsum.words(5),
+      school_id: [*1..10].sample,
+      location_list: Forgery::Address.city,
+      email: "mentor#{i}@hungryhead.org",
+      fund: {balance: 1000},
+      role: 3,
+      market_list: Forgery::Name.industry,
+      settings: {theme: 'danger', idea_notifications: true, feedback_notifications: true, investment_notifications: true, follow_notifications: true, post_notifications: true, weekly_mail: true},
+      confirmed_at: Time.now
+    )
+  }
+
+  1.upto(30) { |i|
+
+    Teacher.create!(
+      name: Forgery::Name.full_name,
+      first_name: Forgery::Name.first_name,
+      last_name: Forgery::Name.last_name,
+      username: Forgery::Internet.user_name + SecureRandom.hex(5),
+      password: 'password',
+      mini_bio: Forgery::LoremIpsum.words(5),
+      school_id: [*1..10].sample,
+      location_list: Forgery::Address.city,
+      email: "teacher#{i}@hungryhead.org",
+      fund: {balance: 1000},
+      role: 4,
+      market_list: Forgery::Name.industry,
+      settings: {theme: 'primary', idea_notifications: true, feedback_notifications: true, investment_notifications: true, follow_notifications: true, post_notifications: true, weekly_mail: true},
+      confirmed_at: Time.now
+    )
+  }
+
+
+  1.upto(100) { |i|
+
+    Student.create!(
+      name: Forgery::Name.full_name,
+      first_name: Forgery::Name.first_name,
+      last_name: Forgery::Name.last_name,
+      username: Forgery::Internet.user_name + SecureRandom.hex(6),
+      password: 'password',
+      mini_bio: Forgery::LoremIpsum.words(5),
+      school_id: [*1..10].sample,
+      location_list: Forgery::Address.city,
+      email: "test#{i}@hungryhead.org",
+      fund: {balance: 1000},
+      role: 1,
+      market_list: Forgery::Name.industry,
+      settings: {theme: 'solid', idea_notifications: true, feedback_notifications: true, investment_notifications: true, follow_notifications: true, post_notifications: true, weekly_mail: true},
+      confirmed_at: Time.now
+    )
+  }
 
 end
---
+++ db/seeds/02_ideas.rb
@@ -0,0 +1,28 @@
+#this is seeds file for loading test schools
+#into db
+ActiveRecord::Base.transaction do
+  1.upto(50) { |i|
+    Idea.create!(
+      name: Forgery::Name.company_name + i.to_s,
+      high_concept_pitch:  Forgery::LoremIpsum.words(5),
+      elevator_pitch: Forgery::LoremIpsum.words(15),
+      description: Forgery::LoremIpsum.words(100),
+      location_list: Forgery::Address.city,
+      fund: {balance: 0},
+      school_id: [*1..10].sample,
+      sections: {
+        market: Forgery::LoremIpsum.paragraphs,
+        problems: Forgery::LoremIpsum.paragraphs,
+        solutions: Forgery::LoremIpsum.paragraphs,
+        vision: Forgery::LoremIpsum.paragraphs,
+        value_proposition: Forgery::LoremIpsum.paragraphs
+      },
+      student_id: [*75..90].sample,
+      looking_for_team: true,
+      rules_accepted: true,
+      market_list: Forgery::Name.industry
+    )
+ }
+
+
+end
