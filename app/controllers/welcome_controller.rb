2f7fc608e90c4d965ad9ec85a299c877213a6474
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2015-06-18 19:12:35 +0100

    removed if condition for welcome controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -30,14 +30,10 @@ class WelcomeController < ApplicationController
   def update
     @welcome = current_user #for merit to avoid console errors
     @user = current_user
-    if @user.rules_accepted?
-      redirect_to root_path
-    else
-      case step
-      when :hello
-        @user.update_attributes(user_params)
-        CreateActivityJob.perform_later(@user.id, "User") if Activity.where(trackable: @user).empty?
-      end
+    case step
+    when :hello
+      @user.update_attributes(user_params)
+      CreateActivityJob.perform_later(@user.id, "User") if Activity.where(trackable: @user).empty?
     end
     sign_in(@user, bypass: true)
     render_wizard @user
