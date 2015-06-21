eef69cd9f9a456589923baca707b07abe54eaedf
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2015-06-21 02:18:22 +0100

    deleted likers and liker partial - old and updated to use more caching for more partials

--- app/helpers/schools_helper.rb
+++ app/helpers/schools_helper.rb
@@ -16,7 +16,7 @@ module SchoolsHelper
     people = school.people_counter.value
     followers = school.followers_counter.value
     ideas = school.ideas_counter.value
-    followed = school.followed_by(current_user)
+    followed = school.followed_by?(current_user)
     "#{school}/people-#{people}/followers-#{followers}/ideas-#{ideas}/followed-#{followed}"
   end
 
