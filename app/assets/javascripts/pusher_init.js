4ffcaaa564fb2970e8e642ceb83417a60a4ab213
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2015-06-21 17:15:22 +0100

    updated pusher init to not include notifications in layouts

--- app/assets/javascripts/pusher_init.js
+++ app/assets/javascripts/pusher_init.js
@@ -2,14 +2,4 @@ if(window.currentUser && window.currentUser.authenticated) {
   var pusher = new Pusher(window.PUSHER_APP_KEY)
   var channel = pusher.subscribe('private-user-' + window.currentUser.id)
   var presence_channel = pusher.subscribe('presence-user-' + window.currentUser.id)
-  if(channel) {
-    channel.bind('new_badge', function(data) {
-     channel.bind('new_badge', function(data) {
-        $('body').pgNotification({style: "simple", message: data.message, position: "top-right", type: "success",timeout: 5000}).show();
-      });
-    });
-    channel.bind('new_notification', function(data){
-      $('body').pgNotification({style: "simple", message: data.data.msg, position: "top-right", type: "success",timeout: 5000}).show();
-    });
-  }
 }
