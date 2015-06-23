224a18e5efe6accb936c8816e9bc6e75de5bf93b
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2015-06-23 02:00:03 +0100

    updated event model and added cache in user

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,8 @@
 class Event < ActiveRecord::Base
 
+  #included modules
+  include Redis::Objects
+
   extend FriendlyId
   friendly_id :slug_candidates
 
@@ -7,5 +10,9 @@ class Event < ActiveRecord::Base
   include Commentable
   include Votable
 
+  list :attendees_ids
+  counter :attendees_counter
+
   belongs_to :eventable, polymorphic: true
+
 end
