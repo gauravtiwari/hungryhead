454ad1a15b12ef323eb4a4a766834bcb60a2caca
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2015-06-27 17:55:50 +0100

    setup redis caching for event attendees

--- app/models/attendee.rb
+++ app/models/attendee.rb
@@ -1,4 +1,21 @@
 class Attendee < ActiveRecord::Base
+
   belongs_to :event
   belongs_to :attendee, class_name: 'User', foreign_key: 'attendee_id'
+
+  after_commit :increment_event_counter, on: :create
+  after_commit :decrement_event_counter, on: :destroy
+
+  private
+
+  def increment_event_counter
+    event.attendees_id << attendee_id
+    event.attendees_counter.increment
+  end
+
+  def decrement_event_counter
+    event.attendees_id.delete(attendee_id)
+    event.attendees_counter.decrement unless event.attendees_counter.value == 0
+  end
+
 end
