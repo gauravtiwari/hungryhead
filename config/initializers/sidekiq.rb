04abbc0ec9ef1c92e775322c2f512e3920cb5d7d
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2015-06-29 02:19:28 +0100

    fixed redis paths for different initializers - 2

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,9 @@
 location = ENV['$REDIS_ADDRESS'] || "localhost"
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: "redis://#{location}:6379/5", namespace: "backgroundjobs" }
+  config.redis = { url: "redis://#{location}:6379/5", namespace: "sidekiq_jobs" }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: "redis://#{location}:6379/5", namespace: "backgroundjobs" }
+  config.redis = { url: "redis://#{location}:6379/5", namespace: "sidekiq_jobs" }
 end
