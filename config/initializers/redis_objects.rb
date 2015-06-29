d259c89a17f0c3faeb9720c6709229cd1c647027
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2015-06-29 02:28:14 +0100

    readded connection pool for redis

--- config/initializers/redis_objects.rb
+++ config/initializers/redis_objects.rb
@@ -1,2 +1,2 @@
 location = ENV['$REDIS_ADDRESS'] || "localhost"
-Redis::Objects.redis = Redis.new( url: "redis://#{location}:6379/1", namespace: "redis_objects")
+Redis::Objects.redis = ConnectionPool.new(size: 5, timeout: 5) { Redis.new( url: "redis://#{location}:6379/1", namespace: "redis_objects") }
