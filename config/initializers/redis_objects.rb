93368945cd502456816ffc12094b140ee80210cf
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2015-06-29 03:45:40 +0100

    reconfigured redis for redis object and soulmate

--- config/initializers/redis_objects.rb
+++ config/initializers/redis_objects.rb
@@ -1,3 +1,1 @@
-location = "redis://#{ENV['$REDIS_ADDRESS']}:6379/3/redis_objects" || 'redis://127.0.0.1:6379/3/redis_objects'
-uri = URI.parse(location)
-Soulmate.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+Redis::Objects.redis = $redis
--- config/initializers/soulmate.rb
+++ config/initializers/soulmate.rb
@@ -1,3 +1,1 @@
-location = "redis://#{ENV['$REDIS_ADDRESS']}:6379/1/soulmate" || 'redis://127.0.0.1:6379/1/soulmate'
-uri = URI.parse(location)
-Soulmate.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+Soulmate.redis = $redis
