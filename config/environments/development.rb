69c91df4fd4e5b4704c1fdf721eb93373724864c
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2015-06-30 22:57:47 +0100

    added connection pool to dalli and redis - 2

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,8 +12,10 @@ Rails.application.configure do
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
-  config.cache_store = :dalli_store, ENV["MEMCACHE_SERVER"], { :namespace => "development", compress: true, :pool_size => 5  }
-  config.identity_cache_store = :dalli_store, ENV["MEMCACHE_SERVER"], { :namespace => "identity_cache_development", compress: true, :pool_size => 5  }
+  config.cache_store = :dalli_store, ENV["MEMCACHE_SERVER"],
+  { :namespace => "development", compress: true, :pool_size => 5  }
+  config.identity_cache_store = :dalli_store, ENV["MEMCACHE_SERVER"],
+  { :namespace => "identity_cache_development", compress: true, :pool_size => 5  }
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
