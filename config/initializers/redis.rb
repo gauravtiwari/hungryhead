971a2cb6cb82e6ee49e80e33b672c684fbc13e60
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2015-06-30 22:34:45 +0100

    fixed redis flushing on start

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,3 @@
-location = "redis://#{ENV['$REDIS_ADDRESS']}:6379/"
+location = "redis://#{ENV['$REDIS_ADDRESS']}:6379/0/"
 uri = URI.parse(location)
 $redis = Redis::Namespace.new("hungryhead", :redis => Redis.new(:host => uri.host, :port => uri.port, :password => uri.password))
