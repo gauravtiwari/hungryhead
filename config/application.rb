09cf07b020a565dfbfb3d93148fe67c5670b221f
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2015-07-09 20:14:12 +0100

    React: Defined react versions to load for react rails

--- config/application.rb
+++ config/application.rb
@@ -16,13 +16,6 @@ Bundler.require(*Rails.groups)
 
 module HungryheadSchoolApp
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
 
     config.assets.paths << File.join(Rails.root, "/vendor/hh")
     config.assets.paths << File.join(Rails.root, "/vendor/assets")
@@ -40,29 +33,25 @@ module HungryheadSchoolApp
     #Background job processing
     config.active_job.queue_adapter = :sidekiq
 
-    #React coding style
-    config.react.jsx_transform_options = {
-      harmony: true,
-      strip_types: true,
+    config.react.server_renderer_pool_size  ||= 1  # ExecJS doesn't allow more than one on MRI
+    config.react.server_renderer_timeout    ||= 20 # seconds
+    config.react.server_renderer = React::ServerRendering::SprocketsRenderer
+    config.react.server_renderer_options = {
+      files: ["react.js", "components.js", "underscore.js", "jquery.js", "bootstrap-tagsinput.js", "js-routes.js", "showdown.js"], # files to load for prerendering
+      replay_console: true,                 # if true, console.* will be replayed client-side
     }
 
-    config.react.max_renderers = 10
-    config.react.timeout = 20 #seconds
-    config.react.react_js = lambda {File.read(::Rails.application.assets.resolve('react.js'))}
-    config.react.component_filenames = ['components.js']
+    config.react.addons = true
 
     # config/application.rb
-
     config.generators do |g|
       g.assets = false
       g.helper = false
       g.view_specs      false
       g.helper_specs    false
     end
+
     config.active_record.schema_format = :sql
 
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
   end
 end
