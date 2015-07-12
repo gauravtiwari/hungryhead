179df991e5858df80ccc3051239c0d7b67c9d85b
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2015-07-13 00:24:06 +0100

    app sidebar routes with engines and browser gem integration into site_feedback

--- vendor/engines/site_feedback/app/controllers/site_feedback/application_controller.rb
+++ vendor/engines/site_feedback/app/controllers/site_feedback/application_controller.rb
@@ -1,6 +1,7 @@
 module SiteFeedback
   class ApplicationController < ActionController::Base
 
+    before_action :set_device_type
     helper_method :user_signed_in?
 
     def user_signed_in?
@@ -9,6 +10,12 @@ module SiteFeedback
       else
         false
       end
+    end
+    private
+
+    def set_device_type
+      request.variant = :phone if browser.mobile?
+      request.variant = :tablet if browser.tablet?
     end
 
   end
