6e726fba81e6d44200bc86a3c59a02c11bf7e30d
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2015-07-15 18:51:02 +0100

    updated event and event invites model

--- app/models/event.rb
+++ app/models/event.rb
@@ -14,16 +14,28 @@ class Event < ActiveRecord::Base
   include Sluggable
   include Commentable
   include Votable
+  include Impressionable
 
   list :attendees_ids
+  list :invites_ids
   counter :attendees_counter
+  counter :invites_counter
 
   belongs_to :owner, polymorphic: true, touch: true
-  has_many :attendees
+  has_many :event_attendees
+  has_many :event_invites
+
+  cache_has_many :event_attendees, embed: true
+  cache_has_many :event_invites, embed: true
+  cache_index :slug
+  cache_index :owner_id, :owner_type
 
   def attending?(user)
     attendees_ids.include?(user.id.to_s)
   end
 
+  def invited?(user)
+    invites_ids.include?(user.id.to_s)
+  end
 
 end
