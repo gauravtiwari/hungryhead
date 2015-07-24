d862cbd67f7a23ad5f54c3abea8b5ac3b150d178
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2015-07-24 01:13:21 +0100

    add: activity json generation concern

---
+++ app/models/concerns/feed_jsonable.rb
@@ -0,0 +1,36 @@
+module FeedJsonable
+
+  extend ActiveSupport::Concern
+
+  def json_blob
+    {
+      id: id,
+      type: self.class.to_s,
+      activity_id: find_activity_id,
+      html: build_html
+    }
+  end
+
+  def build_html
+    root ||= "notifications"
+    path ||= key.to_s.gsub('.', '/')
+    partial_path =  select_path path, root
+    render :partial => partial_path, locals: {activity: self}
+  end
+
+  private
+
+  def find_activity_id
+    if self.class.to_s == "Notification"
+      return parent_id
+    else
+      return uuid
+    end
+  end
+
+  #Get path for notifications or activities
+  def select_path path, root
+    [root, path].map(&:to_s).join('/')
+  end
+
+end
