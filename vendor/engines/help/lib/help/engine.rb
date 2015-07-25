63540ca89504f3b9e0f4db2d3ce752080233cee3
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2015-07-25 20:35:53 +0100

    removed messages helper dependency from engine

--- vendor/engines/help/lib/help/engine.rb
+++ vendor/engines/help/lib/help/engine.rb
@@ -2,7 +2,6 @@ module Help
   class Engine < ::Rails::Engine
     isolate_namespace Help
     config.to_prepare do
-      ApplicationController.helper(::MessagesHelper)
       ApplicationController.helper(::ApplicationHelper)
     end
   end
