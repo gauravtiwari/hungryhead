82e3e4090b707c490ec7feedef29bb9ff5f33738
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2015-07-25 11:19:40 +0100

    removed fields from idea

--- app/assets/javascripts/components/idea/main_content/main_content.js.jsx
+++ app/assets/javascripts/components/idea/main_content/main_content.js.jsx
@@ -29,11 +29,6 @@ var MainContent = React.createClass({
       return (
         <div>
           <Plan idea= {this.state.idea} form= {this.state.form} meta= {this.state.meta} />
-          <Problems idea= {this.state.idea} form= {this.state.form} meta= {this.state.meta} />
-          <Solutions idea= {this.state.idea} form= {this.state.form} meta= {this.state.meta} />
-          <ValueProposition idea= {this.state.idea} form= {this.state.form} meta= {this.state.meta} />
-          <Market idea= {this.state.idea} form= {this.state.form} meta= {this.state.meta} />
-          <BusinessModel idea= {this.state.idea} form= {this.state.form} meta= {this.state.meta} />
         </div>
     )
   }
