e000190302cbf87014158126188357db166e8102
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2015-07-31 00:18:52 +0100

    school seed file update

--- db/seeds/00_schools.rb
+++ db/seeds/00_schools.rb
@@ -7,100 +7,101 @@ ActiveRecord::Base.transaction do
   School.create!(
     name: 'Lancaster University',
     location_list: 'Lancaster',
-    description: Forgery::LoremIpsum.words(20),
-    email: 'admin@lancs.ac.uk',
-    domain: 'lancs.ac.uk',
-    user_id: 2,
-    website_url: 'lancs.ac.uk'
+    description: "We’re ranked as the best university in the North West by the Guardian, Sunday Times and Complete University Guide.
+    We’ve also won the 'Best University Halls' award for the last six years running, and have been named the best value for money university in England.",
+    email: 'admin@lancaster.ac.uk',
+    domain: 'lancaster.ac.uk',
+    user_id: 1,
+    website_url: 'lancaster.ac.uk'
   )
 
   School.create!(
     name: 'Manchester University',
     location_list: 'Manchester',
-    description: Forgery::LoremIpsum.words(20),
-    email: 'admin@mancs.ac.uk',
-    domain: 'mancs.ac.uk',
-    user_id: 2,
-    website_url: 'mancs.ac.uk'
+    description: "Our vision at The University of Manchester is to make our institution one of the top 25 research universities in the world by 2020.",
+    email: 'admin@manchester.ac.uk',
+    domain: 'manchester.ac.uk',
+    user_id: 1,
+    website_url: 'manchester.ac.uk'
   )
 
   School.create!(
     name: 'Imperial College London',
     location_list: 'London',
-    description: Forgery::LoremIpsum.words(20),
-    email: 'admin@icl.ac.uk',
-    domain: 'icl.ac.uk',
-    user_id: 2,
-    website_url: 'icl.ac.uk'
+    description: "Imperial is a science-based institution, consistently rated amongst the world's best universities.",
+    email: 'admin@imperial.ac.uk',
+    domain: 'imperial.ac.uk',
+    user_id: 1,
+    website_url: 'imperial.ac.uk'
   )
 
   School.create!(
     name: 'Cambridge University',
     location_list: 'Cambridge',
-    description: Forgery::LoremIpsum.words(20),
+    description: "With more than 18,000 students from all walks of life and all corners of the world, nearly 9,000 staff, 31 Colleges and 150 Departments, Faculties, Schools and other institutions, no two days are ever the same at the University of Cambridge.",
     email: 'admin@cam.ac.uk',
     domain: 'cam.ac.uk',
-    user_id: 3,
+    user_id: 1,
     website_url: 'cam.ac.uk'
   )
 
   School.create!(
     name: 'Salford University',
     location_list: 'Salford',
-    description: Forgery::LoremIpsum.words(20),
+    description: "The University of Salford is a friendly, vibrant and pioneering University. We continually invest in our campus, facilities and industry partnerships to enhance your student experience and provide opportunities to develop the skills needed to succeed in your career",
     email: 'admin@salford.ac.uk',
     domain: 'salford.ac.uk',
-    user_id: 4,
+    user_id: 1,
     website_url: 'salford.ac.uk'
   )
 
   School.create!(
     name: 'University of central Lancashire',
     location_list: 'Preston',
-    description: Forgery::LoremIpsum.words(20),
+    description: "The University of Central Lancashire (UCLan) in Preston was founded in 1828 as the Institution for the Diffusion of Knowledge.",
     email: 'admin@uclan.ac.uk',
     domain: 'uclan.ac.uk',
-    user_id: 2,
+    user_id: 1,
     website_url: 'uclan.ac.uk'
   )
 
   School.create!(
     name: 'Manchester Metropolitan University',
     location_list: 'Manchester',
-    description: Forgery::LoremIpsum.words(20),
+    description: "Manchester Metropolitan University was awarded university status in 1992 and is part of the largest higher education campus in the UK and one of the most extensive education centres in Europe.",
     email: 'admin@mmu.ac.uk',
     domain: 'mmu.ac.uk',
-    user_id: 2,
+    user_id: 1,
     website_url: 'mmu.ac.uk'
   )
 
   School.create!(
     name: 'Oxford University',
     location_list: 'Oxford',
-    description: Forgery::LoremIpsum.words(20),
-    email: 'admin@oxford.ac.uk',
-    domain: 'oxford.ac.uk',
-    user_id: 2,
-    website_url: 'oxford.ac.uk'
+    description: "Oxford is a world-leading centre of learning, teaching and research and the oldest university in the English-speaking world.",
+    email: 'admin@ox.ac.uk',
+    domain: 'ox.ac.uk',
+    user_id: 1,
+    website_url: 'ox.ac.uk'
   )
 
   School.create!(
     name: 'University College London',
     location_list: 'London',
-    description: Forgery::LoremIpsum.words(20),
+    description: "UCL was founded in 1826 to open up university education in England to those who had been excluded from it. In 1878, it became the first university in England to admit women students on equal terms with men.",
     email: 'admin@ucl.ac.uk',
     domain: 'ucl.ac.uk',
-    user_id: 2,
+    user_id: 1,
     website_url: 'ucl.ac.uk'
   )
 
   School.create!(
     name: 'Bristol University',
     location_list: 'Bristol',
-    description: Forgery::LoremIpsum.words(20),
+    description: "Bristol University is internationally renowned, ranked in the top 30 universities globally (QS World University Rankings), due to its outstanding teaching and research, its superb facilities and highly talented students and staff.",
     email: 'admin@bristol.ac.uk',
     domain: 'bristol.ac.uk',
-    user_id: 2,
+    user_id: 1,
     website_url: 'bristol.ac.uk'
   )
 
