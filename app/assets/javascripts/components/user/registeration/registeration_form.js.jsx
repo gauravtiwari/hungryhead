f6d1b53916af546e10d583fab2f3993ef661575c
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2015-07-30 18:22:35 +0100

    fix logic for username suggestion

--- app/assets/javascripts/components/user/registeration/registeration_form.js.jsx
+++ app/assets/javascripts/components/user/registeration/registeration_form.js.jsx
@@ -54,7 +54,9 @@ var RegisterationForm = React.createClass({
   onUsernameChange: function(e) {
     data = {
       username: e.target.value,
-      name: this.refs.name.getDOMNode().value.trim()
+      first_name: this.refs.first_name.getDOMNode().value.trim()
+      last_name: this.refs.last_name.getDOMNode().value.trim()
+      name: this.refs.first_name.getDOMNode().value.trim() + ' ' + this.refs.last_name.getDOMNode().value.trim()
     }
     var target = e.target;
     if($(this.refs.name.getDOMNode().length) != 0) {
