598d06337537dc00d5ef8537621581e80343af6f
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2015-07-30 20:29:31 +0100

    deleted deploy hooks

--- .cloud66/dbseed.sh
+++ .cloud66/dbseed.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 cd $STACK_PATH
+redis-cli flushall
 bundle exec rake environment db:drop && bundle exec rake db:create && bundle exec rake db:migrate
 bundle exec rake db:seed
+bundle exec rake db:seed_questions
--- .cloud66/scripts/rake_task.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd $STACK_PATH
-redis-cli flushall
-bundle exec rake db:seed_questions
