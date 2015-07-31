78b56ca7770d5ef6767af1551dbd84afa2ef52d2
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2015-07-31 14:07:24 +0100

    invite request controller

--- app/controllers/invite_requests_controller.rb
+++ app/controllers/invite_requests_controller.rb
@@ -7,6 +7,7 @@ class InviteRequestsController < ApplicationController
   # POST /invite_requests.json
   def create
     @invite_request = InviteRequest.new(invite_request_params)
+    @invite_request.request = request
     respond_to do |format|
       if @invite_request.save
         format.js {render :show}
