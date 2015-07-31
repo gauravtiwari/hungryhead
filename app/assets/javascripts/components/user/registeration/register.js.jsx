425a19eba7002c89ab382ca02a0267ec2396cb1b
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2015-07-31 13:30:30 +0100

    updated register form and get_started

--- app/assets/javascripts/components/user/registeration/register.js.jsx
+++ app/assets/javascripts/components/user/registeration/register.js.jsx
@@ -20,6 +20,7 @@ var Register = React.createClass({
       success: function ( data ) {
         if(data.name) {
           $('#form-register').remove();
+
           $('body').pgNotification({style: "simple", message: "Registeration Successful. You will receive an email shortly on " + data.email + ". Redirecting...", position: "top-right", type: "success",timeout: 5000}).show();
           setTimeout(function(){
             window.location.href = Routes.root_path;
