4cde8d2172ae1c7346a88ce9bb34940b008ae0e8
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2015-08-04 18:57:23 +0100

    added mixpanel tracking for links and meta events

--- app/controllers/invite_requests_controller.rb
+++ app/controllers/invite_requests_controller.rb
@@ -8,6 +8,9 @@ class InviteRequestsController < ApplicationController
   def create
     @invite_request = InviteRequest.new(invite_request_params)
     @invite_request.request = request
+    if Rails.env.production?
+      meta_events_tracker.event!(:user, :new_invite_request, { :name => @invite_request.name, user_type: @invite_request.user_type, :email => @invite_request.email, :id => @invite_request.id })
+    end
     respond_to do |format|
       if @invite_request.save
         format.js {render :show}
--- config/meta_events.rb
+++ config/meta_events.rb
@@ -3,6 +3,7 @@ global_events_prefix :ab
 version 1, "2015-07-26" do
   category :user do
     event :signed_up, "2015-07-26", "user creates a brand-new account"
+    event :new_invite_request, "2015-07-26", "user request an invite to create account"
   end
   category :visitor do
     event :page_viewed, "2015-07-26", "visitor visits a page"
