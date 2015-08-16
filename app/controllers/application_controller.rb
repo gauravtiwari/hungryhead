e178fb4b5b39ee9bb6323b7b6a2b7004ff820ee6
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2015-08-16 17:56:16 +0100

    changed hash key case

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -39,7 +39,7 @@ class ApplicationController < ActionController::Base
   end
 
   def track_visitor_events
-    meta_events_tracker.event!(:visitor, :page_viewed, { :ip => request.ip, browser: browser.name, :title => params[:action].humanize.capitalize, url: request.original_url, referrer_url: request.referer })
+    meta_events_tracker.event!(:visitor, :page_viewed, { :ip => request.ip, :Browser => browser.name, :Title => params[:action].humanize.capitalize, :Url => request.original_url, referrer_url: request.referer })
   end
 
   def meta_events_tracker
