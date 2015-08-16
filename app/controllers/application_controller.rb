6fa9ee3b5ca1ce4c82645ac02c6ca0a4b5781024
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2015-08-16 17:48:15 +0100

    application controller tracking - add url

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -39,7 +39,7 @@ class ApplicationController < ActionController::Base
   end
 
   def track_visitor_events
-    meta_events_tracker.event!(:visitor, :page_viewed, { :ip => request.ip, :title => params[:action].humanize.capitalize })
+    meta_events_tracker.event!(:visitor, :page_viewed, { :ip => request.ip, :title => params[:action].humanize.capitalize, request_url: request.original_url, referrer_url: request.referer })
   end
 
   def meta_events_tracker
