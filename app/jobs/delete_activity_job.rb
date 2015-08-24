e56f2f7d3ce16e619185748b64e7374ce8b4416d
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2015-08-24 09:54:19 +0100

    update gems and using arel for activities

--- app/jobs/delete_activity_job.rb
+++ app/jobs/delete_activity_job.rb
@@ -2,7 +2,9 @@ class DeleteActivityJob < ActiveJob::Base
   def perform(trackable_id, trackable_type)
     ActiveRecord::Base.connection_pool.with_connection do
       @trackable = trackable_type.constantize.find(trackable_id)
-      Activity.where(trackable: @trackable).find_each do |activity|
+      activities = Arel::Table.new(:activities)
+      activities.where(activities[:trackable_id].eq(trackable_id).and(activities[:trackable_type].eq(trackable_type)).or(activities[:recipient_id].eq(trackable_id).and(activities[:recipient_type].eq(trackable_type))))
+      activities.find_each do |activity|
         #Delete cached activities
         DeleteNotificationCacheService.new(activity).delete
         #finally destroy the activity
