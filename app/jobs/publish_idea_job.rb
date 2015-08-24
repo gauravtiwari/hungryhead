aab754f5b6cf11e8d69249483b75f718f0ec79bb
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2015-08-24 17:31:21 +0100

    stop sending feed from idea job

--- app/jobs/publish_idea_job.rb
+++ app/jobs/publish_idea_job.rb
@@ -36,16 +36,6 @@ class PublishIdeaJob < ActiveJob::Base
 
       Idea.trending.remrangebyrank(0, -20)
 
-      # Send notifications to followers
-      User.find(@user.followers_ids.members).each do |f|
-        #Send notifications to followers
-        Pusher.trigger_async("private-user-#{f.uid}",
-          "new_ticker_item",
-          {data: @activity.user.ticker.rangebyscore(@activity.created_at.to_i + @activity.id, @activity.created_at.to_i + @activity.id)}.to_json
-        )
-        #send mail to users if subscribed
-        #IdeaMailer.new_idea(@idea, @user, f).deliver_later if f.idea_notifications && f.idea_notifications == true
-      end
     end
   end
 
