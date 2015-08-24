1bba55a8c4faaa4102c55edb96e23d340a3ec2aa
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2015-08-24 22:30:26 +0100

    refactored user admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,10 +13,8 @@ class User < ActiveRecord::Base
   include Redis::Objects
 
   #Callbacks
-  before_create :add_fullname, if: :name_not_present?
-  before_create :add_username, if: :username_absent?
-  before_destroy :remove_from_soulmate, :decrement_counters, unless: :is_admin
-  before_create :seed_fund, :seed_settings, unless: :is_admin
+  before_create :add_fullname, :add_username, :seed_fund, :seed_settings
+  before_destroy :remove_from_soulmate, :decrement_counters
 
   #Call Service to update cache
   after_save :soulmate_loader, if: :rebuild_cache?
@@ -272,6 +270,7 @@ class User < ActiveRecord::Base
   end
 
   def add_username
+    return if username.present?
     email_username = self.name.parameterize
     if User.find_by_username(email_username).blank?
       email_username = email_username
@@ -287,17 +286,20 @@ class User < ActiveRecord::Base
 
   # returns and adds first_name and last_name to database
   def add_fullname
+    return if first_name.present?
     self.first_name = firstname
     self.last_name =  lastname
   end
 
   #Seeds amount into database on: :create
   def seed_fund
+    return if admin?
     self.fund = {balance: 1000}
   end
 
   #Seeds settings into database on: :create
   def seed_settings
+    return if admin?
     self.settings = {
       theme: "#{role.downcase}",
       idea_notifications: true,
@@ -318,12 +320,14 @@ class User < ActiveRecord::Base
   end
 
   def remove_from_soulmate
+    return if admin?
     #Remove search index if :record destroyed
     loader = Soulmate::Loader.new("people")
     loader.remove("id" => id)
   end
 
   def decrement_counters
+    return if admin?
     #Decrement counters
     school.people_counter.reset
     school.people_counter.incr(User.from_school(school_id).size) if school_id.present?
