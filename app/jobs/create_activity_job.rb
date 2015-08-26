8afdff1065f8ec62af57357d8e84145c95acab64
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2015-08-26 02:00:16 +0100

    update jobs

--- app/jobs/create_activity_job.rb
+++ app/jobs/create_activity_job.rb
@@ -2,11 +2,11 @@ class CreateActivityJob < ActiveJob::Base
 
   def perform(trackable_id, trackable_type)
     ActiveRecord::Base.connection_pool.with_connection do
-      if trackable_type.constantize.find(trackable_id).present?
+      if trackable_type.constantize.where(trackable_id: trackable_id).empty?
+        return false
+      else
         @trackable = trackable_type.constantize.find(trackable_id)
         "Create#{trackable_type}NotificationService".constantize.new(@trackable).create
-      else
-        return false
       end
     end
   end
--- app/jobs/update_activity_job.rb
+++ app/jobs/update_activity_job.rb
@@ -2,13 +2,13 @@ class UpdateActivityJob < ActiveJob::Base
 
   def perform(user_id)
     ActiveRecord::Base.connection_pool.with_connection do
-      if User.find(user_id).present?
+      if User.where(id: user_id).empty?
+        return false
+      else
         @user = User.find(user_id)
         Activity.where(user: @user).find_each do |activity|
           UpdateNotificationCacheService.new(activity).update
         end
-      else
-        return false
       end
     end
   end
